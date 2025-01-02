75721026ed29bb786cfcff1344672629f2fa95d1
DimaBaziuk <mitrobaz@gmail.com>
2025-01-02 16:56:09 +0200

    remove uslles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import "./App.css";
-
 function App() {
-    return (
-        <>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    );
+    return <p>Click on the Vite and React logos to learn more</p>;
 }
 
 export default App;
